7fa33757e20885ff076681b0e6b19f0587d45465
Schendsa <sam.van.schendel@hotmail.nl>
2014-09-10 15:39:53 +0200

    euler4-2

--- euler4.php
+++ euler4.php
@@ -1,3 +1,16 @@
 <?php
-
+$number5=0;
+for ($number=999; $number>100 ; $number--) { 
+	for ($number2=999; $number2 >100 ; $number2--) { 
+		$number3=$number*$number2;
+		$number4=strrev($number3);
+		if ($number3==$number4) {
+			if ($number4>$number5) {
+				$number5=$number4;
+				echo $number5;
+				echo "<br/>";
+			}
+		}
+	}
+}
 ?>
