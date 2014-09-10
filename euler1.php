731b6877fd9f13b493a1c613cdb3cdd8075736aa
Schendsa <sam.van.schendel@hotmail.nl>
2014-09-10 13:00:03 +0200

    euler1-3

--- euler1.php
+++ euler1.php
@@ -3,15 +3,14 @@
 	for ($nummer=1; $nummer <1000 ; $nummer++) {
 		if ($nummer%3==0) {
 			$sum=$sum+$nummer;
-			echo "Nummer " . $nummer;
+			echo $nummer;
 			echo "<br/>";
-			echo "Sum " . $sum;
 		}
 		elseif ($nummer%5==0) {
 			$sum=$sum+$nummer;
-			echo "Nummer " . $nummer;
+			echo $nummer;
 			echo "<br/>";
-			echo "Sum " . $sum;
 		}
 	}
+	echo "Dit is de som van de even getallen " . $sum;
 ?>
