1bd4508d0c14aca85da6327627ca8640d62cefc5
Schendsa <sam.van.schendel@hotmail.nl>
2014-09-15 10:02:25 +0200

    euler5-1

---
+++ euler5.php
@@ -0,0 +1,46 @@
+<?php
+for ($number1=2520; $number1 <10000000000 ; $number1++) { 
+	if ($number1%1==0) {
+		if ($number1%2==0) {
+			if ($number1%3==0) {
+				if ($number1%4==0) {
+					if ($number1%5==0) {
+						if ($number1%6==0) {
+							if ($number1%7==0) {
+								if ($number1%8==0) {
+									if ($number1%9==0) {
+										if ($number1%10==0) {
+											if ($number1%11==0) {
+												if ($number1%12==0) {
+													if ($number1%13==0) {
+														if ($number1%14==0) {
+															if ($number1%15==0) {
+																if ($number1%16==0) {
+																	if ($number1%17==0) {
+																		if ($number1%18==0) {
+																			if ($number1%19==0) {
+																				if ($number1%20==0) {
+																					echo $number1;
+																					echo "<br/>";
+																				}
+																			}
+																		}
+																	}
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+?>
